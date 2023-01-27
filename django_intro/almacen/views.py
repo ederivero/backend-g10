03e09c77d85f3a15b58963f23eb05d9e76b772e9
Paolo Soncco <paholo2054@gmail.com>
2023-01-26 21:55:12 -0500

    actualizar categorias

--- django_intro/almacen/views.py
+++ django_intro/almacen/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ProductosModel, CategoriasModel
 from .serializers import ProductosSerializer, CategoriasSerializer
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 
 def renderHtml(request):
@@ -24,24 +24,44 @@ class CategoriasView(generics.GenericAPIView):
         try:
             record = self.get_queryset()
             serializer = self.get_serializer(record, many=True)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({
-                'message': 'Internal server error',
-                'error': str(e)
-            })
+            return Response(
+                data = {
+                    'message': 'Internal server error',
+                    'error': str(e)
+                }, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
     def post(self, request):
         try:
             categoria = self.get_serializer(data=request.data)
             if categoria.is_valid():
                 categoria.save()
-                print(categoria.data)
                 return Response(categoria.data)
+
+            error = 'Faltan campos'
+            for campo in categoria.errors:
+                error = error + ' ' + campo + ', '
             return Response({
-                
+                'message': error
             })
-        except:
+        except Exception as e:
             return Response({
-                'message': 'Internal server error'
-            })
+                'message': 'Internal server error',
+                'error': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class ActualizarCategoriasView(generics.GenericAPIView):
+    queryset = CategoriasModel.objects.all()
+    serializer_class = CategoriasSerializer
+
+    def update(self, request, id):
+        try:
+            pass
+        except Exception as e:
+            return Response({
+                'message': 'Internal server error',
+                'error': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
