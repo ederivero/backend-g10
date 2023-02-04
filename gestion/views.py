0441aec2019d3360cc31eef1749e50ade0b63e87
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2023-02-03 20:51:04 -0500

    avance django

--- gestion/views.py
+++ gestion/views.py
@@ -33,7 +33,31 @@ class PlatoApiView(ListCreateAPIView):
         # SELECT * FROM platos WHERE disponibilidad = true;
         resultado = PlatoModel.objects.filter(disponibilidad=True).all()
         print(resultado)
-        
+        # aca llamamos al serializer y le pasamos la informacion proveniente de la bd y con el parametro many True indicamos que le estamos pasando un arreglo de instancias
+        serializador = PlatoSerializer(instance=resultado, many=True)
+        print(serializador.data)
+
         return Response(data= {
-            'message': 'Me hizo get'
+            'content': serializador.data
+        })
+    
+
+    def post(self, request:Request):
+        body = request.data
+        # cuando queremos verificar si la informacion entrante es valida entonces utilizamos el parametro data en vez del parametro instance
+        serializador = PlatoSerializer(data=body)
+
+        # es el encargado de validar si la data es correcta y cumple con todos los requisitos
+        valida = serializador.is_valid()
+
+        if valida == False:
+            return Response(data={
+                'message': 'La informacion es invalida',
+                # error > mostrar los errores SOLAMENTE cuando la data no sea valida
+                'content': serializador.errors
+
+            })
+
+        return Response(data={
+            'message': 'Plato creado exitosamente'
         })
